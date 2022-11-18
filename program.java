d5958c105bb944627afd5a0336e249a14a89d30a
Viktoria <viktoriapredko1@gmail.com>
2022-11-18 12:43:00 +0300

    Solve task

---
+++ program.java
@@ -0,0 +1,28 @@
+// 1.Напишите метод, который принимает на вход строку (String) и определяет является ли строка палиндромом (возвращает boolean значение).
+	
+	// import java.util.Scanner;
+	// public class program{
+	//     public static void main(String[] args) {
+	//         Scanner iScanner = new Scanner(System.in);
+	//         System.out.println("Строка: ");
+	//         String str = iScanner.nextLine();
+	            
+	//         boolean result = method(str);
+	//         System.out.println(result);
+	//     }
+	
+
+	//     private static boolean method(String str) {
+	//         char[] array = str.toCharArray();
+	//         int i2 = array.length-1;
+	//         for(int i=0; i< array.length; i++){
+	//             if (array[i] != array[i2]){
+	//                 return false;
+	//             }
+	//             else{
+	//                 i2--;
+	//             }
+	//         }  
+	//     return true;
+	//     }  
+	// }    
